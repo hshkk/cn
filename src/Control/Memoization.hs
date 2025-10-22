037b795cba15669b2411d4ba761692282b8b3a2d
hshkk <190803096+hshkk@users.noreply.github.com>
2025-10-21 20:02:25 -0700

    Revision to spacing in `Control.Recursive.Levenshtein`.

--- src/Control/Memoization.hs
+++ src/Control/Memoization.hs
@@ -3,4 +3,5 @@ module Control.Memoization where
 import Control.Monad.State (State)
 import Data.Map            (Map)
 
+-- | The memoization type.
 type M s a = State (Map s a) a
--- src/Control/Recursive/Levenshtein.hs
+++ src/Control/Recursive/Levenshtein.hs
@@ -15,14 +15,14 @@ d l@(x:xs) r@(y:ys)
                 , d xs ys ] + 1
 
 d' :: Int -> Int -> String -> String -> M (Int,Int) Int
-d' xs      0 _ _ = return xs
-d' 0      ys _ _ = return ys
-d' i j l r =
+d' i 0  _ _ = return i
+d' 0  j _ _ = return j
+d' i  j  l r =
     get >>= \st ->
     case M.lookup (i,j) st of
         Just n  -> return n
         Nothing -> 
-            (let li=i-1; lj=j-1 in
+            (let li = i-1; lj = j-1 in
             if l !! li == r !! lj 
             then d' li lj l r
             else d' li j  l r >>= \del ->
